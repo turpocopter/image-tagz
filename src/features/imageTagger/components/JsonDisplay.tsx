f93d66afd96c22a97aea7946ccbb3544c77ada32
Rémy Turpault <remy.turpault@gmail.com>
2021-04-27 23:37:17 +0200

    Fixed json display and link url generation for performance

--- src/features/imageTagger/components/JsonDisplay.tsx
+++ src/features/imageTagger/components/JsonDisplay.tsx
@@ -1,17 +1,30 @@
 import React from "react";
 import { useAppSelector } from "../../../app/hooks";
-import { getJsonData } from "../imageTaggerSlice";
+import { getJsonTags, getImageData } from "../imageTaggerSlice";
 
 const JsonDisplay = () => {
-	const jsonData = useAppSelector(getJsonData);
+	const jsonTags = useAppSelector(getJsonTags);
+	const imageData = useAppSelector(getImageData);
+
+	const handleClick = (e: any) => {
+		if (imageData) {
+			e.target.href = `data:text/json;charset=utf-8,${encodeURIComponent(
+				JSON.stringify(imageData, null, 2)
+			)}`;
+		}
+	};
+
 	return (
 		<div>
-			<textarea value={jsonData} readOnly />
-			<a
-				href={`data:text/json;charset=utf-8,${encodeURIComponent(jsonData)}`}
-				download='taggedImage.json'>
-				Go
-			</a>
+			<textarea className='jsonDisplay' rows={15} value={jsonTags} readOnly />
+			{
+				<a
+					onClick={handleClick}
+					href='./index.html'
+					download='taggedImage.json'>
+					Go
+				</a>
+			}
 		</div>
 	);
 };
